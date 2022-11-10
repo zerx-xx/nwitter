f2e04b0ed1db0776e5e5106804fff9a1016d336e
밀가루 <drizzle@milgaluui-MacBookAir.local>
2022-11-10 18:34:56 +0900

    sytle add twitter logo

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,13 +2,16 @@ import React from "react";
 import {Link} from "react-router-dom" ;
 import HomeLogo from "/Users/drizzle/nwitter/src/imag/home logo.png" ;
 import ProfileLogo from "/Users/drizzle/nwitter/src/imag/profile.png" ;
+import twitterlogo from "/Users/drizzle/nwitter/src/imag/twitter logo.png" ;
 import "/Users/drizzle/nwitter/src/css/navigation.moule.css" ; 
+
 
 const Navigation = ({userObj}) => {
     return(
         <div className="listUl">
             <nav className="nav">
                 <ul>
+                <img className="twitterlogo" src={twitterlogo} width="38" height="33" />
                     <li className="homeTitle">
                         <Link to="/"> <img src={HomeLogo} width="40" height="40"/></Link>
                     </li>
